7ab3b312bc4a50c63ecf233e7323abff0a0212aa
andrewvnukov <andrix.ork@gmail.com>
2024-01-09 13:30:49 +0300

    New

---
+++ Cafe_Managment/Autorization.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cafe_Managment
+{
+    /// <summary>
+    /// Логика взаимодействия для Autorization.xaml
+    /// </summary>
+    public partial class Autorization : Window
+    {
+        public Autorization()
+        {
+            InitializeComponent();
+            MainMenu window = new MainMenu();
+            window.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Cafe_Managment/ViewModel/NavigationVM.cs
@@ -0,0 +1,51 @@
+﻿using Cafe_Managment.Utilities;
+using Cafe_Managment.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Cafe_Managment.ViewModel
+{
+    class NavigationVM : ViewModelBase
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand DishCommand { get; set; }
+        public ICommand EmployeeCommand { get; set; }
+        public ICommand KitchenCommand { get; set; }
+        public ICommand MenuCommand { get; set; }
+        public ICommand OrderCommand { get; set; }
+        public ICommand ProfileCommand { get; set; }
+        public ICommand StatisticCommand { get; set; }
+
+        private void Dish(object obj) => CurrentView = new DishVM();
+        private void Employee(object obj) => CurrentView = new EmployeeVM();
+        private void Kitchen(object obj) => CurrentView = new KitchenVM();
+        private void Menu(object obj) => CurrentView = new MenuVM();
+        private void Profile(object obj) => CurrentView = new ProfileVM();
+        private void Order(object obj) => CurrentView = new OrderVM();
+        private void Statistic(object obj) => CurrentView = new StatisticVM();
+
+        public NavigationVM()
+        {
+            DishCommand = new RelayCommand(Dish);
+            EmployeeCommand = new RelayCommand(Employee);
+            KitchenCommand = new RelayCommand(Kitchen);
+            MenuCommand = new RelayCommand(Menu);
+            OrderCommand = new RelayCommand(Order);
+            ProfileCommand = new RelayCommand(Profile);
+            StatisticCommand = new RelayCommand(Statistic);
+            
+            CurrentView = new ProfileVM();
+        }
+
+    }
+}
