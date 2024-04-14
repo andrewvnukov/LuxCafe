eefc9f50a0321d3f5114d6641570389b92383fc1
andrewvnukov <andrix.ork@gmail.com>
2024-04-14 22:42:41 +0300

    ++

--- Cafe_Managment/ViewModel/OrderVM.cs
+++ Cafe_Managment/ViewModel/OrderVM.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using Cafe_Managment.Controls;
 using Cafe_Managment.Utilities;
 
@@ -10,8 +12,9 @@ namespace Cafe_Managment.ViewModel
 {
     internal class OrderVM : ViewModelBase
     {
-
+        int _selectedCategory;
         List<Category> _categoryList;
+        List<Dish> _dishList;
         public List<Category> CategoryList
         {
             get { return _categoryList; }
@@ -19,13 +22,34 @@ namespace Cafe_Managment.ViewModel
                 OnPropertyChanged(nameof(CategoryList));
             }
         }
+        public int SelectedCategory
+        {
+            get { return _selectedCategory; }
+            set { _selectedCategory = value; OnPropertyChanged(nameof(SelectedCategory)); }
+        }
+
+        public List<Dish> DishList
+        {
+            get { return _dishList; }
+            set { _dishList = value; OnPropertyChanged(nameof(DishList)); }
+        }
+
+        public ICommand SwitchToCategoryCommand { get; set; }
+
         public OrderVM() 
         {
+            SwitchToCategoryCommand = new RelayCommand(ExecuteSwitchToCategoryCommand);
+
             CategoryList = new List<Category>();
             CategoryList.Add(new Category("Завтраки", "/Images/Categories/Breakfast.png"));
             CategoryList.Add(new Category("Завтраки", "/Images/Categories/Breakfast.png"));
             CategoryList.Add(new Category("Завтраки", "/Images/Categories/Breakfast.png"));
             CategoryList.Add(new Category("Завтраки", "/Images/Categories/Breakfast.png"));
+        }
+
+        private void ExecuteSwitchToCategoryCommand(object obj)
+        {
+            MessageBox.Show(SelectedCategory.ToString());
         }
     }
 }
